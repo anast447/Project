1d055e8c852f5bcd8d3679e192345990cb1e1fc9
anast447 <eminem1998dg@mail.ru>
2025-02-17 18:27:16 +0800

    Сделала задание 8

---
+++ src/ReplaceText.java
@@ -0,0 +1,29 @@
+public class ReplaceText {
+    public static void main(String[] args) {
+        String text = "Привет Влад, ты лабы по джаве сделал?";
+        int k = 3;
+        char replacementChar = '$';
+
+        String modifiedText = replace(text, k, replacementChar);
+
+        System.out.println("Текст: " + text);
+        System.out.println("Измененный текст: " + modifiedText);
+    }
+
+    public static String replace(String text, int k, char replacementChar) {
+        String[] words = text.split(" ");
+        StringBuilder modifiedText = new StringBuilder();
+
+        for (String word : words) {
+            if (k > word.length()) {
+                modifiedText.append(word);
+            }
+                else {
+                    String modifiedWord = word.substring(0, k - 1) + replacementChar + word.substring(k);
+                    modifiedText.append(modifiedWord);
+                }
+            modifiedText.append(" ");
+        }
+        return modifiedText.toString().trim();
+    }
+}
